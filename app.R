62407ddac6bbaee60ab916a27c1d3d8b774c2e2d
Arnau Giribet PyCharm <arnaugiribetmarch@gmail.com>
2023-08-22 15:12:41 +0200

    Primera prova

--- app.R
+++ app.R
@@ -1,8 +1,11 @@
 library(shiny)
+library(highcharter)
+library(dplyr)
+library(tidyr)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 options(OutDec = ',', digits=8)
 source('r/utils.R')
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
 
     # Application title
@@ -11,39 +14,92 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-          strong(p("Habitatge", style = "font-size:20px")),
-          numericInput("preu", "Preu de l'habitatge", value=300000, min=0,step=10000),
-          numericInput("taxacio", "Taxació", value=425, min=0),
-          numericInput("notaria", "Notaria", value=936, min=0),
-          numericInput("registre", "Registre de la propietat", value=478, min=0),
-          numericInput("gestoria", "Gestoria", value=300, min=0),
-          shinyWidgets::currencyInput("itp", "Impost de Transmisió de Patrimoni", value = 0.05, format = "percentageEU2dec"),
+          strong(p("Ingressos i despeses actuals", style = "font-size:20px")),
+          fluidRow(
+            column(4,
+                   shinyWidgets::currencyInput("ingressos", "Ingressos anuals nets", value=30000, align='left')),
+            column(4,
+                   shinyWidgets::currencyInput("lloguer", "Cost mensual del lloguer", value=408, align='left')),
+            column(4,
+                   shinyWidgets::currencyInput("costVida", "Altres despeses anuals", value=11000, align='left'))
+          ),
+          br(),
+          strong(p("Compra d'habitatge", style = "font-size:20px")),
+          fluidRow(
+            column(5,
+                   shinyWidgets::currencyInput("preu", "Preu de l'habitatge", value=280000, align='left'))
+          ),
+          fluidRow(
+            column(5,
+                   shinyWidgets::currencyInput("taxacio", "Taxació", value=425, align='left')),
+            column(5,
+                   shinyWidgets::currencyInput("notaria", "Notaria", value=936, align='left'))
+          ),
+          fluidRow(
+            column(5,
+                   shinyWidgets::currencyInput("registre", "Registre de la propietat", value=478, align='left')),
+            column(5,
+                   shinyWidgets::currencyInput("gestoria", "Gestoria", value=300, align='left'))
+          ),
+          fluidRow(
+            column(5,
+                   shinyWidgets::currencyInput("itp", "Impost sobre Transmissions Patrimonials", value = 0.05, 
+                                               format = "percentageEU2dec", align='left'))
+          ),
           br(),
           strong(p("Hipoteca", style = "font-size:20px")),
-          shinyWidgets::currencyInput("interes", "Interès", value = 0.027, format = "percentageEU2dec"),
-          numericInput("anys", "Anys", value=30, min=0),
-          numericInput("entrada", "Entrada", value=60000, min=0, step=10000),
-          
+          fluidRow(
+            column(4,
+                   shinyWidgets::currencyInput("entrada", "Entrada", value=60000, align='left')),
+            column(4,
+                   shinyWidgets::currencyInput("interes", "Interès", value = 0.03, format = "percentageEU2dec", align='left')),
+            column(4,
+                   numericInput("anys", "Anys", value=30, min=0))
+          ),
+          br(),
+          strong(p("Despeses del nou habitatge", style = "font-size:20px")),
+          fluidRow(
+            column(4,
+                   shinyWidgets::currencyInput("upfrontImprovements", "Inversió inicial", value=0, align='left')),
+            column(4,
+                   shinyWidgets::currencyInput("manteniment", "Manteniment anual", value=1500, align='left'))
+          ),
+          br(),
+          strong(p("Venda del futur habitatge", style = "font-size:20px")),
+          fluidRow(
+            column(5,
+                   shinyWidgets::currencyInput("cancelacioHipoteca", "Penalització per cancel·lació anticipada d'hipoteca", 
+                                               value = 0.01, format = "percentageEU2dec", align='left')),
+            column(5,
+                   shinyWidgets::currencyInput("honoraris", "Honoraris de l'agència immobiliària", 
+                                               value = 0.05, format = "percentageEU2dec", align='left'))
+          ),
+          fluidRow(
+            column(5,
+                   shinyWidgets::currencyInput("incrementValor", "Increment anual de valor de l'habitatge", 
+                                               value = 0.02, format = "percentageEU2dec", align='left'))
+          )
         ),
-
-        # Show a plot of the generated distribution
+        
         mainPanel(
           strong(p("Hipoteca Resultant", style = "font-size:20px")),
+          textOutput('costTotal'),
           textOutput('hipoteca'),
           textOutput('interesHipoteca'),
           textOutput('quotaMensual'),
           br(),
-          strong(p("Evolució del patrimoni", style = "font-size:20px")),
-          plotOutput("valuePlot")
+          highchartOutput("hipotecaPlot", height = "500px"),
+          br(),
+          strong(p("Evolució del patrimoni i els estalvis", style = "font-size:20px")),
+          highchartOutput("estalvisPlot", height = "500px")
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
+  cost <- reactive(input$preu+input$taxacio+input$notaria+input$registre+input$gestoria+input$itp*input$preu)
   hipoteca<-reactive({
-    cost <- reactive(input$preu+input$taxacio+input$notaria+input$registre+input$gestoria+input$itp*input$preu)
     f_hipoteca(cost(),input$entrada)
   })
   quotaMensual<-reactive({
@@ -53,41 +109,45 @@ server <- function(input, output) {
     round(quotaMensual()*12*input$anys-hipoteca(),2)
   })
 
-  output$hipoteca<-renderText(paste0("La hipoteca sense interès és de: ",hipoteca(),"€"))
-  output$interesHipoteca<-renderText(paste0("L'interès és de: ",interesHipoteca(), "€"))
-  output$quotaMensual<-renderText(paste0("Quota mensual resultant: ",quotaMensual(), "€"))
+  output$costTotal<-renderText(paste0("El cost total de la compra és de: ",f(cost())," €"))
+  output$hipoteca<-renderText(paste0("La hipoteca sense interès és de: ",f(hipoteca())," €"))
+  output$interesHipoteca<-renderText(paste0("L'interès és de: ",f(interesHipoteca()), " €"))
+  output$quotaMensual<-renderText(paste0("Quota mensual resultant: ",f(quotaMensual()), " €"))
+
+  hipotecaRestantAnual<-reactive({
+    f_hipotecaRestant(hipoteca(),input$anys,input$interes,quotaMensual())
+  })
   
+  dades<-reactive({
+    f_estalvi(input$ingressos,input$lloguer,input$costVida,
+              input$anys,input$entrada,quotaMensual(),hipotecaRestantAnual(),
+              input$upfrontImprovements,input$manteniment,
+              input$cancelacioHipoteca,input$honoraris,input$incrementValor,
+              input$preu,cost())
+  })
   
-  estalvi_abans_anys<-c(0, 9780, 19560, 29340, 39120, 48900, 58680, 68460, 78240, 88020, 
-                        97800, 107580, 117360, 127140, 136920, 146700, 156480, 166260, 
-                        176040, 185820, 195600, 205380, 215160, 224940, 234720, 244500, 
-                        254280, 264060, 273840, 283620, 293400)
-  nou_estalvi_anys<-c(-100000, -95799.83, -91599.67, -87399.5, -83199.34, -78999.17, 
-                      -74799, -70598.84, -66398.67, -62198.51, -57998.34, -53798.18, 
-                      -49598.01, -45397.84, -41197.68, -36997.51, -32797.35, -28597.18, 
-                      -24397.01, -20196.85, -15996.68, -11796.52, -7596.35, -3396.19, 
-                      803.98, 5004.15, 9204.31, 13404.48, 17604.64, 21804.81, 26004.98)
-  nou_patrimoni_anys<-c(-22375.39, -13683.67, -4878.36, 4043.4, 13084.57, 22248.16, 
-                        31537.26, 40955.06, 50504.8, 60189.82, 70013.55, 79979.49, 90091.23, 
-                        100352.48, 110766.99, 121338.66, 132071.45, 142969.45, 154036.82, 
-                        165277.85, 176696.93, 188298.57, 200087.38, 212068.1, 224245.58, 
-                        236624.79, 249210.83, 262008.94, 275024.48, 288262.94, 301729.98)
-                      
-  anys_t<-0:30
-  estalvi<-100000
-  t<-15
+  output$hipotecaPlot <- renderHighchart({
+    dades() %>% 
+      select(Any,`Hipoteca Restant`,`Valor Amortitzat Hipoteca`, `Quota Pagada`) %>% 
+      pivot_longer(cols      = -Any, # works similar to using select()
+                   names_to  = 'Grup', # the name of the column that will have column names as labels
+                   values_to = 'Quantitat'  # the name of the column for the values
+                   ) %>% 
+    hchart('line', hcaes(x=Any,y=Quantitat,group=Grup)) %>%
+      hc_add_theme(hc_theme_elementary())
+  })
   
-    output$valuePlot <- renderPlot({
-      t<-15
-      plot(estalvi_abans_anys~anys_t, ylim=c(min(-estalvi,0),400000), pch='.', xlim=c(0,t), xaxt='n', yaxt='n', ylab='')
-      grid()
-      axis(1, at = seq(0, t, by = 1), las=1)
-      axis(2, at = seq(0, 10^6, by = 10^5), las=2)
-      lines(estalvi_abans_anys~anys_t)
-      #lines(cost_lloguer_actual_anys~anys_t, col='red', lty=3)
-      lines(nou_estalvi_anys~anys_t,col='green', lty=3)
-      lines(nou_patrimoni_anys~anys_t,col='blue', lty=3)
-    })
+  output$estalvisPlot <- renderHighchart({
+    dades() %>% 
+      select(Any,`Estalvi Previ`,`Nou Estalvi`,`Valor Liquidat Habitatge (menys despeses de venda)`,`Nou Patrimoni Total`) %>% 
+      pivot_longer(cols      = -Any, # works similar to using select()
+                   names_to  = 'Grup', # the name of the column that will have column names as labels
+                   values_to = 'Quantitat'  # the name of the column for the values
+      ) %>% 
+      hchart('line', hcaes(x=Any,y=Quantitat,group=Grup)) %>%
+      hc_add_theme(hc_theme_elementary())
+  })
+    
 }
 
 # Run the application 
--- r/utils.R
+++ r/utils.R
@@ -1,3 +1,12 @@
+roundup2 <- function(x, digits=0) {
+  posneg = sign(x)
+  z = abs(x)*10^digits
+  z = z + 0.5 + sqrt(.Machine$double.eps)
+  z = trunc(z)
+  z = z/10^digits
+  z*posneg
+}
+
 f_hipoteca <- function(cost,entrada){
   
   if(cost<=entrada){
@@ -17,3 +26,80 @@ f_quotaMensual <- function(hipoteca,interes,anys){
   }
   return(round(quotaMensual,2))
 }
+
+f_hipotecaRestant <- function(hipoteca,anys,interes,quota_mensual){
+  
+  #mensual (es necessita per l'anual)
+  interes_mensual<-c()
+  amortitzacio_mensual<-c()
+  for(mes in 1:(anys*12)){
+    interes_mensual[mes]<-(hipoteca-sum(amortitzacio_mensual))*interes/12
+    amortitzacio_mensual[mes]<-quota_mensual-interes_mensual[mes]
+  }
+  hipoteca_restant_mensual<-hipoteca-cumsum(amortitzacio_mensual)
+  
+  #anual
+  interes_anual<-c()
+  amortitzacio_anual<-c()
+  for(any in 1:anys){
+    interes_anual[any]<-sum(interes_mensual[(((any-1)*12+1):(any*12))])
+    amortitzacio_anual[any]<-sum(amortitzacio_mensual[(((any-1)*12+1):(any*12))])
+  }
+  
+  amortitzacioAnual<-roundup2(c(0,amortitzacio_anual),2)
+  amortitzacioAcumulada<-roundup2(cumsum(amortitzacioAnual))
+  hipotecaRestantAnual<-roundup2(hipoteca-amortitzacioAcumulada,2)
+  
+  return(list(hipotecaRestantAnual=hipotecaRestantAnual,
+              amortitzacioAnual=amortitzacioAnual,
+              amortitzacioAcumulada=amortitzacioAcumulada))
+}
+  
+  
+f <- function(x) {
+  format(x,big.mark = '.',decimal.mark = ',', nsmall=2)
+}
+
+f_estalvi <- function(ingressos,lloguer,costVida,
+                      anys,entrada,quotaMensual,hipotecaRestantAnual,
+                      upfrontImprovements,manteniment,
+                      cancelacioHipoteca,honoraris,incrementValor,
+                      preu,cost){
+  
+  #valors inicial
+  habitatge_amortitzat_inicial<-entrada-(cost-preu)
+  
+  #valors constants al temps
+  estalvi_previ<-ingressos-12*lloguer-costVida
+  nou_estalvi<-ingressos-12*quotaMensual-manteniment-costVida
+  
+  #sèries anuals
+  estalvi_previ_anual<-roundup2(entrada+upfrontImprovements+estalvi_previ*(0:anys),2)
+  nou_estalvi_anual<-roundup2(nou_estalvi * (0:anys), 2)
+  habitatge_amortitzat_anual<-habitatge_amortitzat_inicial+hipotecaRestantAnual$amortitzacioAcumulada
+  quota_anual_acumulada=roundup2(12*quotaMensual*(0:anys),2)
+  preu_anual<-roundup2(preu*(incrementValor+1)**(0:anys))
+  despeses_venda_anual<-preu_anual*(honoraris)+(hipotecaRestantAnual$hipotecaRestantAnual*cancelacioHipoteca)
+  valor_habitatge_post_venda<-roundup2(preu_anual-hipotecaRestantAnual$hipotecaRestantAnual-despeses_venda_anual,2)
+
+  dades<-data.frame(`Estalvi Previ`= estalvi_previ_anual,
+                    `Nou Estalvi` = nou_estalvi_anual,
+                    `Hipoteca Restant` = hipotecaRestantAnual$hipotecaRestantAnual,
+                    `Valor Amortitzat Hipoteca` = hipotecaRestantAnual$amortitzacioAcumulada,
+                    `Quota Pagada` = quota_anual_acumulada,
+                    `Valor Liquidat Habitatge (menys despeses de venda)`=valor_habitatge_post_venda,
+                    `Any` = (0:anys), check.names = F) %>%
+    mutate(`Nou Patrimoni Total` = `Valor Liquidat Habitatge (menys despeses de venda)` + `Nou Estalvi`, .before=Any)
+  
+  return(dades)
+}
+
+
+
+
+
+
+
+
+
+
